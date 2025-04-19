e992bf83f5b2ad231a33a8102d3d9fab68580da6
Abdulra66hmn <examunv@gmail.com>
2025-04-20 00:23:46 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// open & close menu
+
+var menu = document.querySelector('#menu');
+
+function open_menu() {
+    menu.classList.add("active")
+}
+function close_menu() {
+    menu.classList.remove("active")
+}
